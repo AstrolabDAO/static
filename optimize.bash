dd1c9d54a36c5497cdb490f6a636fe761ffec5aa
derpa <paul@astrolab.fi>
2025-01-30 00:27:14 +0700

    missing stables

--- optimize.bash
+++ optimize.bash
@@ -1,2 +1,8 @@
 #!/bin/bash
-find ./static/assets/images -type d -exec svgo --multipass -f {} --config=./svgo.config.js \;
+find ./static/assets/images -type d -exec svgo --multipass -f {} --config=./svgo.config.js 2>&1 \; | \
+  grep -B2 "%" | \
+  grep -v "Done in" | \
+  grep -v "^--$" | \
+  grep -v " 0% " | \
+  paste - - | \
+  awk '{if ($0 ~ /%/ && $0 !~ /0%/) print $1 $4 " " $5 " " $6 " " $7 "Kb ✔️"}'
